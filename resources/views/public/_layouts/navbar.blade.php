51126a71563e2d55770b185700bb7ba2de33d435
OusmaneNabie <nabieousmane2@gmail.com>
2024-03-07 22:26:19 -0800

    implémentation du template  public.page-d'accueil

---
+++ resources/views/public/_layouts/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav id="navbar" class="navbar">
+    <ul>
+      <li>
+        <a class="nav-link scrollto active" href="#hero">Accueil</a>
+      </li>
+      <li><a class="nav-link scrollto" href="#about">A propos</a></li>
+      <li>
+        <a class="nav-link scrollto" href="#portfolio">Portfolio</a>
+      </li>
+      <li><a class="nav-link scrollto" href="#team">Equipe</a></li>
+      <li><a href="blog.html">Evènement</a></li>
+      <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+  <!-- .navbar -->
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\public\AcueilController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AcueilController::class,'index'])->name('accueil');
